094757fc53df21d64e0696f99fd3d454a9bb92aa
Boozy090121 <132027776+Boozy090121@users.noreply.github.com>
2025-03-31 09:48:50 -0400

    Delete Dashboard v5 directory

--- Dashboard v5/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module './ExcelProcessor' {
-  export default class NovoNordiskExcelProcessor {
-    constructor();
-    processExcelFile(fileData: any, fileType: string): void;
-    getProcessedData(): any;
-  }
-}
-
-// Add any other module declarations as needed 
