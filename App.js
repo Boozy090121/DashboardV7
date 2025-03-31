dc7e7e79f773b992d205069330ab79bd6063d3d6
Boozy090121 <132027776+Boozy090121@users.noreply.github.com>
2025-03-31 12:09:47 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NovoNordiskDashboard from './novo-nordisk-dashboard';
-import { DataProvider } from './DataContext';
+import { DataProvider } from './DataContext.js';
 
 // Main App component - Directly wraps the dashboard with DataProvider
 const App = () => {
